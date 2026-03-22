ee7a9315a9889b56654c46683e445a7876045c7e
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-22 09:54:26 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,39 +1,52 @@
 import os
 
 class Config(object):
-    # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
-    # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    # Get these values from my.telegram.org
-    # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    # Banned Unwanted Members..
+
+    # Bot Token 
+    TG_BOT_TOKEN = ""8799486461:AAH6td7_S68a3PnGNJermUgxA6_A1vhBS-c"
+
+    # Telegram API
+    APP_ID = 34724970
+    API_HASH = "f240eae7c60e8e30c17203ab0e052f7e"
+
+    # Authorized User
+    AUTH_USERS = {7521421400}
+
+    # Banned Users
     BANNED_USERS = []
-    # the download location, where the HTTP Server runs
+
+    # Download location
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
+
+    # File size limits
     MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 1572864000
     FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    # https://t.me/hevcbay/951
+
+    # Chunk size
+    CHUNK_SIZE = 128
+
+    # Default thumbnail
+    DEF_THUMB_NAIL_VID_S = "https://placehold.it/90x90"
+
+    # Proxy (optional)
+    HTTP_PROXY = ""
+
+    # Shortener API (optional)
     OUO_IO_API_KEY = ""
-    # maximum message length in Telegram
+
+    # Telegram message max length
     MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
+
+    # Timeout
     PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
+
+    # Watermark file
     DEF_WATER_MARK_FILE = ""
-    # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+
+    # Database
+    DB_URI = "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0"
+
+    # Chatbase (optional)
+    CHAT_BASE_TOKEN = ""
 
